c7695e233f2e1710ca7d83ab3197d9f8b036942a
Marley <mpsvideos.marley@gmail.com>
2024-05-14 17:16:04 -0300

    correção do menu hamburguer na página login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,19 @@
+// Interatividade na barra de navegação //
+// Este trecho de código é executado quando o DOM é totalmente carregado
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona o elemento com a classe 'hamburger', que geralmente é o ícone do menu de hambúrguer
+    const hamburger = document.querySelector('.hamburger');
+    // Seleciona o elemento com a classe 'nav-menu', que é o menu de navegação
+    const navMenu = document.querySelector('.nav-menu');
+  
+    // Adiciona um ouvinte de evento de clique ao ícone do menu de hambúrguer
+    hamburger.addEventListener('click', function () {
+        // Quando o ícone do menu de hambúrguer é clicado, a classe 'show' é alternada no menu de navegação,
+        // tornando-o visível ou oculto, dependendo do seu estado atual
+        navMenu.classList.toggle('show');
+    });
+  });
+
 // capturando elementos no DOM
 // usuario
 let usuarioInput = document.getElementById("usuario");
